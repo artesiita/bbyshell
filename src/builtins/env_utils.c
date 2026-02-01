55033ea401067729acc545ca6a60d24b0185bd81
Laia Artes Sebastia <lartes-s@cbr1s6.42barcelona.com>
2026-02-01 13:48:50 +0100

    lexer i proves

---
+++ src/builtins/env_utils.c
@@ -0,0 +1,26 @@
+
+#include "../../inc/minishell.h"
+
+void	free_env_list(t_env *head)
+{
+	t_env	*temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->key);
+        free(temp->value);
+		free(temp);
+	}
+}
+
+/*
+t_env   *create_env_node(char *content)
+{
+    t_env   *new;
+
+    new = (t_env *)malloc(sizeof(t_env));
+    if (!new)
+        return (NULL);
+}*/
---
+++ src/lexer/lexer_utils.c
@@ -0,0 +1,52 @@
+#include "../../inc/minishell.h"
+
+int is_space(char c)
+{
+    if (c == ' ')
+        return (1);
+    return (0);
+}
+
+int is_redirection(char c)
+{
+    if (c == '<' || c == '>' || c == '|')
+        return (1);
+    return (0);
+}
+
+void free_token_list(t_token *head)
+{
+    t_token *temp;
+
+    while (head)
+    {
+        temp = head;
+        head = head->next;
+        if (temp->content)
+            free(temp->content);
+        free(temp);
+    }
+}
+
+void    add_token(t_token **head, char *content, t_token_type type, t_quote_ctx quote)
+{
+    t_token *new;
+    t_token *last;
+
+    new = malloc(sizeof(t_token));
+    if (!new)
+        return;
+    new->content = content;
+    new->type = type;
+    new->quote = quote;
+    new->next = NULL;
+    if (*head == NULL)
+        *head = new;
+    else
+    {
+        last = *head;
+        while (last->next != NULL)
+            last = last->next;
+        last->next = new;
+    }
+}
