ffd4b9d23b189831f1908d55452b31cfa65e4ded
Laia Artes Sebastia <lartes-s@cbr1s6.42barcelona.com>
2026-02-01 13:39:12 +0100

    header i makefile

---
+++ Makefile
@@ -0,0 +1,49 @@
+NAME = minishell
+SRC = src/main.c \
+		src/builtins/env_setup.c \
+		src/builtins/env_utils.c \
+		src/lexer/lexer.c \
+		src/lexer/lexer_utils.c \
+		src/proves.c
+
+
+LIBFT_DIR = libft
+LIBFT = $(LIBFT_DIR)/libft.a
+
+OBJ_DIR = obj
+OBJ = $(SRC:%.c=$(OBJ_DIR)/%.o)
+DEP = $(SRC:%.c=$(OBJ_DIR)/%.d)
+
+CC = cc
+CCFLAGS = -Wall -Wextra -Werror -g #-fsanitize=address
+LDFLAGS = -lreadline
+
+all: libft $(NAME)
+
+-include $(DEP)
+
+$(NAME): $(LIBFT) $(OBJ)
+	$(CC) $(CCFLAGS) $(OBJ) $(LIBFT) -o $(NAME) $(LDFLAGS)
+
+libft:
+	$(MAKE) -C $(LIBFT_DIR)
+
+$(OBJ_DIR)/%.o: %.c $(HEADER) | $(OBJ_DIR)
+	mkdir -p $(dir $@)
+	$(CC) $(CCFLAGS) -MMD -MP -I $(HEADER_DIR)/ -Ilibft -c $< -o $@
+
+$(OBJ_DIR):
+	mkdir -p $(OBJ_DIR)
+
+clean:
+	rm -rf $(OBJ_DIR) $(OBJ_DIR_BONUS)
+	$(MAKE) -C $(LIBFT_DIR) clean
+
+fclean: clean
+	rm -f $(NAME) $(NAME_BONUS)
+	$(MAKE) -C $(LIBFT_DIR) fclean
+
+re: fclean all
+
+
+.PHONY: all bonus clean fclean re libft
---
+++ inc/minishell.h
@@ -0,0 +1,115 @@
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+/*-----------------------------------------------------------------*/
+/*                             INCLUDES                            */
+/*-----------------------------------------------------------------*/
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <signal.h>
+# include <sys/wait.h>
+# include <sys/types.h>
+# include <errno.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "../libft/libft.h"
+
+/*-----------------------------------------------------------------*/
+/*                             STRUCTS                             */
+/*-----------------------------------------------------------------*/
+
+/*============= LEXER ================*/
+
+typedef enum e_token_type
+{
+	T_WORD,
+	T_PIPE,
+	T_REDIR_IN,      /* <  */
+	T_REDIR_OUT,     /* >  */
+	T_REDIR_HEREDOC, /* << */
+	T_REDIR_APPEND   /* >> */
+}	t_token_type;
+
+typedef enum e_quote_ctx
+{
+	Q_NONE,
+	Q_SINGLE,
+	Q_DOUBLE
+}	t_quote_ctx;
+
+typedef struct s_token
+{
+	char            *content;
+	t_token_type    type;
+	t_quote_ctx     quote;
+	struct s_token  *next;
+}	t_token;
+
+/*============= PARSER ================*/
+
+typedef enum e_redir_type
+{
+	R_IN,       /* <  */
+	R_OUT,      /* >  */
+	R_APPEND,   /* >> */
+	R_HEREDOC   /* << */
+}	t_redir_type;
+
+typedef struct s_redir
+{
+	t_redir_type    type;
+	char            *target;
+	struct s_redir  *next;
+}	t_redir;
+
+typedef struct s_cmd
+{
+	char		**argv;
+	t_redir     *redirs;
+}	t_cmd;
+
+typedef struct s_pipeline
+{
+	int		cmd_count;
+	t_cmd   **cmds;
+}	t_pipeline;
+
+/*============= GLOBAL ================*/
+
+extern volatile sig_atomic_t	g_signal;
+
+typedef struct s_env
+{
+	char		    *key;
+	char		    *value;
+	struct s_env    *next;
+}	t_env;
+
+typedef struct s_mini
+{
+	t_env       *env;
+	int         exit_status;
+	t_token     *tokens;
+	t_pipeline  *pipeline;
+}	t_mini;
+
+/*-----------------------------------------------------------------*/
+/*                           PROTOTYPES                            */
+/*-----------------------------------------------------------------*/
+
+/*============= ENV ================*/
+t_env   *env_setup(char **env);
+void	free_env_list(t_env *head);
+
+/*============= LEXER ================*/
+int		is_redirection(char c);
+int		is_space(char c);
+void    add_token(t_token **head, char *content, t_token_type type, t_quote_ctx quote);
+void	free_token_list(t_token *head);
+t_token *lexer(char *input);
+
+#endif
