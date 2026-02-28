10779b3e421da2650fe848d76a96ff5619b9f069
Laia Artes Sebastia <lartes-s@car9s6.42barcelona.com>
2026-02-28 16:34:11 +0100

    fix: env_setup: functions less than 25 lines

--- src/builtins/env_setup.c
+++ src/builtins/env_setup.c
@@ -6,7 +6,7 @@
 /*   By: lartes-s <lartes-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/13 17:33:14 by lartes-s          #+#    #+#             */
-/*   Updated: 2026/02/27 18:20:03 by lartes-s         ###   ########.fr       */
+/*   Updated: 2026/02/28 16:32:43 by lartes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -54,30 +54,26 @@ int	append_env_node(t_env **head, t_env **cur, char *content)
 
 int	env_setup(t_mini *mini, char **env)
 {
-	t_env	*head;
-	t_env	*cur;
-	int		i;
+	int	i;
 
 	if (!env || !*env)
 		return (0);
-	head = NULL;
-	cur = NULL;
+	mini->env_head = NULL;
+	mini->env_cur = NULL;
 	i = 0;
 	if (env && *env)
 	{
 		while (env[i])
 		{
-			if (!append_env_node(&head, &cur, env[i]))
-				return (free_env_list(head), 0);
+			if (!append_env_node(&mini->env_head, &mini->env_cur, env[i]))
+				return (free_env_list(mini->env_head), 0);
 			i++;
 		}
 	}
 	else
 	{
-		if (!append_env_node(&head, &cur, ""))
-			return (free_env_list(head), 0);
+		if (!append_env_node(&mini->env_head, &mini->env_cur, ""))
+			return (free_env_list(mini->env_head), 0);
 	}
-	mini->env_head = head;
-	mini->env_cur = cur;
 	return (1);
 }
