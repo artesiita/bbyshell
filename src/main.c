7729279becde9164709c78eb753dbf4eeda1735a
Laia Artes Sebastia <lartes-s@car9s4.42barcelona.com>
2026-03-22 13:09:14 +0100

    header de ft_free_split.c

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: laiaartes <laiaartes@student.42.fr>        +#+  +:+       +#+        */
+/*   By: lartes-s <lartes-s@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/13 17:23:57 by lartes-s          #+#    #+#             */
-/*   Updated: 2026/03/21 17:44:31 by laiaartes        ###   ########.fr       */
+/*   Updated: 2026/03/22 13:02:46 by lartes-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,7 +35,7 @@ int	main(int ac, char **av, char **env)
 		{
 			add_history(line);
 			mini.tokens = lexer(line);
-			print_tokens(mini.tokens);
+			// print_tokens(mini.tokens);
 			expansions(&mini);
 			print_tokens(mini.tokens);
 			if (mini.tokens)
